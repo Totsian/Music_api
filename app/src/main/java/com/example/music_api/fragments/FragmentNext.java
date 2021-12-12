1f047662c27d2a1719b031c2520492db8bae924e
tatsian <tanya@bk.ru>
2021-12-12 22:34:11 +0300

    add FragmentNext

--- app/src/main/java/com/example/music_api/fragments/FragmentNext.java
+++ app/src/main/java/com/example/music_api/fragments/FragmentNext.java
@@ -1,4 +1,41 @@
 package com.example.music_api.fragments;
 
-public class FragmentNext {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.music_api.OnClickListenerFragment;
+import com.example.music_api.R;
+
+public class FragmentNext extends Fragment {
+    OnClickListenerFragment onSelectedButtonListener;
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+        try {
+            onSelectedButtonListener = (OnClickListenerFragment) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement onSomeEventListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View nextV = inflater.inflate(R.layout.fragment_next, container, false);
+
+        return nextV;
+    }
 }
