a67e6ec4da738feea38deea2482d72148690692a
Totsian <tanyaherondale@gmail.com>
2021-12-15 12:22:24 +0300

    update

--- app/src/main/java/com/example/music_api/RetrofitBuilder.java
+++ app/src/main/java/com/example/music_api/RetrofitBuilder.java
@@ -1,22 +1,18 @@
 package com.example.music_api;
 
 import com.example.music_api.interfaces.AnimeApi;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitBuilder {
     static final String BASE_URL = "https://ghibliapi.herokuapp.com";
+
     public static AnimeApi getApi() {
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         AnimeApi animeApi = retrofit.create(AnimeApi.class);
